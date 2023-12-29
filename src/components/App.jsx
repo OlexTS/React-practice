76ca98309bbb2e8863eef5d352a14888fdeaddfe
OlexTS <sashokseven@gmail.com>
2023-12-29 17:42:54 +0200

    created counter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 // import { Card } from './Card/Card'
 import { Header } from "./Header/Header";
-import { Counter } from "./Counter/Counter";
+// import Counter from "./Counter/Counter";
+import { Component } from "react";
+import Modal from "./Modal/Modal";
 
-const App = () => {
-  return (
-    // <Card isOnline/>
-    <div className="container">
-      <Header />
-      <Counter />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    isOpenModal: false,
+  };
+  showModal = () => {
+    this.setState({ isOpenModal: true });
+  };
+  closeModal = () => {
+    this.setState({ isOpenModal: false });
+  };
+  render() {
+    return (
+      <div className="container">
+        <Header showModal={this.showModal} />
+        {/* <Counter /> */}
+
+        {this.state.isOpenModal && <Modal closeModal={this.closeModal} >Hello I'm Modal</Modal>}
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,23 +1,44 @@
-export const Counter = () => {
-  return (
-    <div className="position-absolute top-50 start-50 translate-middle">
-      <div className="card bg-dark text-white " style={{ width: "600px" }}>
-        <div className="card-body">
-          <h5 className="card-title text-center fs-1">Counter</h5>
-          <p
-            className="card-text  text-center"
-            style={{ fontSize: "80px" }}
-          >0</p>
-          <div className="d-flex justify-content-center px-5">
-            <button className="btn btn-outline-success me-5">
-              <i className="bi bi-plus-circle fs-1"></i>
-            </button>
-            <button className="btn  btn-outline-danger ms-5">
-              <i className="bi bi-dash-circle fs-1"></i>
-            </button>
+import { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+  handleIncrement = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
+  handleDecrement = () => {
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
+  };
+  render() {
+    const { count } = this.state;
+    return (
+      <div className="position-absolute top-50 start-50 translate-middle">
+        <div className="card bg-dark text-white " style={{ width: "600px" }}>
+          <div className="card-body">
+            <h5 className="card-title text-center fs-1">Counter</h5>
+            <p className="card-text  text-center" style={{ fontSize: "80px" }}>
+              {count}
+            </p>
+            <div className="d-flex justify-content-center px-5">
+              <button
+                className="btn btn-outline-success me-5"
+                onClick={this.handleIncrement}
+              >
+                <i className="bi bi-plus-circle fs-1"></i>
+              </button>
+              <button
+                className="btn  btn-outline-danger ms-5"
+                onClick={this.handleDecrement}
+              >
+                <i className="bi bi-dash-circle fs-1"></i>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+export default Counter;
