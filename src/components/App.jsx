21a0233cef4ec0738ba95df53f7d0613e4d7d1f8
OlexTS <sashokseven@gmail.com>
2024-01-02 23:05:24 +0200

    comp. form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import { nanoid } from "nanoid";
+import { Component } from "react";
 // import { Card } from './Card/Card'
 import { Header } from "./Header/Header";
 // import Counter from "./Counter/Counter";
-import { Component } from "react";
 import Modal from "./Modal/Modal";
 import ToDoList from "./ToDoList/TodoList";
+import FormLogin from "./FormLogin/FormLogin";
 
 class App extends Component {
   state = {
@@ -16,13 +17,28 @@ class App extends Component {
   closeModal = () => {
     this.setState({ isOpenModal: false });
   };
+  createUser = (user) => {
+    const newUser = {
+      ...user,
+      id: nanoid(),
+    };
+    console.log(newUser);
+  };
+
   render() {
     return (
       <div className="container">
         <Header showModal={this.showModal} />
         {/* <Counter /> */}
-<ToDoList/>
-        {this.state.isOpenModal && <Modal closeModal={this.closeModal} >Hello I'm Modal</Modal>}
+        <ToDoList />
+        {this.state.isOpenModal && (
+          <Modal closeModal={this.closeModal}>
+            <FormLogin
+              createUser={this.createUser}
+              closeModal={this.closeModal}
+            />
+          </Modal>
+        )}
       </div>
     );
   }
---
+++ src/components/FormRegistration/FormRegistration.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class FormRegistration extends Component{
+    state = {}
+    render() {
+        return <></>
+    }
+}
+export default FormRegistration
