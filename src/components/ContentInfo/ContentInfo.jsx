a49dd18b210a07474ea31109ea947dde89786510
OlexTS <sashokseven@gmail.com>
2024-01-08 23:20:39 +0200

    added spinner

--- src/components/ContentInfo/ContentInfo.jsx
+++ src/components/ContentInfo/ContentInfo.jsx
@@ -4,27 +4,34 @@ import { nanoid } from "nanoid";
 class ContentInfo extends Component {
   state = {
     news: null,
+    isLoading: false,
   };
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.searchText !== this.props.searchText) {
-      getNews(this.props.searchText)
+      this.setState({ isLoading: true });
+      setTimeout(()=>{getNews(this.props.searchText)
         .then((response) => response.json())
-        .then((data) => this.setState({ news: data.articles }));
+        .then((data) => this.setState({ news: data.articles }))
+        .finally(this.setState(() => ({ isLoading: false })));},3000)
+      
     }
   }
   render() {
-    const { news } = this.state;
+    const { news, isLoading } = this.state;
 
     return (
       <>
+        {isLoading && (
+          <div className="text-center">
+  <div className="spinner-border" role="status">
+    <span className="visually-hidden">Loading...</span>
+  </div>
+</div>
+        )}
         <ul>
           {news &&
             news.map((el) => {
-              return (
-                <li key={nanoid()}>
-                  {el.title}
-                </li>
-              );
+              return <li key={nanoid()}>{el.title}</li>;
             })}
         </ul>
       </>
