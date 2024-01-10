00d0cef860c0c14890419d8d76ca8b69103c5cb8
OlexTS <sashokseven@gmail.com>
2024-01-10 23:21:19 +0200

    refactoring App on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,88 @@
+import { useState } from "react";
 import { nanoid } from "nanoid";
-import { Component } from "react";
 // import { Card } from './Card/Card'
 import { Header } from "./Header/Header";
 // import Counter from "./Counter/Counter";
-// import Modal from "./Modal/Modal";
+import Modal from "./Modal/Modal";
 // import ToDoList from "./ToDoList/TodoList";
-// import FormLogin from "./FormLogin/FormLogin";
+import FormLogin from "./FormLogin/FormLogin";
 import Search from "./Search/Search";
 import ContentInfo from "./ContentInfo/ContentInfo";
 
-class App extends Component {
-  state = {
-    isOpenModal: false,
-    searchText: ''
-  };
+const App = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  
+  const showModal = () => setIsOpenModal(true);
 
- 
-  showModal = () => {
-    this.setState({ isOpenModal: true });
-  };
-  closeModal = () => {
-    this.setState({ isOpenModal: false });
-  };
-  createUser = (user) => {
+  const closeModal = () => setIsOpenModal(false);
+
+  const createUser = (user) => {
     const newUser = {
       ...user,
       id: nanoid(),
     };
     console.log(newUser);
   };
-  handleSearch = (searchText) => {
-    this.setState({searchText})
-  }
+  const handleSearch = (searchText) => setSearchText(searchText);
 
-  render() {
-    return (
-      <div className="container">
-        <Header showModal={this.showModal} />
-        <Search handleSearch={this.handleSearch} />
-        <ContentInfo searchText={this.state.searchText} />
-        {/* <Counter /> */}
-        {/* <ToDoList />
-        {this.state.isOpenModal && (
-          <Modal closeModal={this.closeModal}>
-            <FormLogin
-              createUser={this.createUser}
-              closeModal={this.closeModal}
-            />
-          </Modal>
-        )} */}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="container">
+      <Header showModal={showModal} />
+      <Search handleSearch={handleSearch} />
+      <ContentInfo searchText={searchText} />
+      {/* <Counter /> */}
+      {/* <ToDoList /> */}
+      {isOpenModal && (
+        <Modal closeModal={closeModal}>
+          <FormLogin createUser={createUser} closeModal={closeModal} />
+        </Modal>
+      )}
+    </div>
+  );
+};
+// class App extends Component {
+//   state = {
+//     isOpenModal: false,
+//     searchText: ''
+//   };
+
+//   showModal = () => {
+//     this.setState({ isOpenModal: true });
+//   };
+//   closeModal = () => {
+//     this.setState({ isOpenModal: false });
+//   };
+//   createUser = (user) => {
+//     const newUser = {
+//       ...user,
+//       id: nanoid(),
+//     };
+//     console.log(newUser);
+//   };
+//   handleSearch = (searchText) => {
+//     this.setState({searchText})
+//   }
+
+//   render() {
+//     return (
+//       <div className="container">
+//         <Header showModal={this.showModal} />
+//         <Search handleSearch={this.handleSearch} />
+//         <ContentInfo searchText={this.state.searchText} />
+//         {/* <Counter /> */}
+//         {/* <ToDoList />
+//         {this.state.isOpenModal && (
+//           <Modal closeModal={this.closeModal}>
+//             <FormLogin
+//               createUser={this.createUser}
+//               closeModal={this.closeModal}
+//             />
+//           </Modal>
+//         )} */}
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
