a434f494e944cede921406307f646ee6cb821c55
OlexTS <sashokseven@gmail.com>
2024-01-15 20:13:46 +0200

    refactoring search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,40 +1,68 @@
-import { Component } from "react";
+import { useState } from "react";
 
+const Search = ({ handleSearch }) => {
+  const { value, setValue } = useState("");
+  const handleChange = ({ target: { value } }) => {
+    setValue(value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.handleSearch(this.state.value);
+  };
 
-class Search extends Component{
-    state = {
-        value: ''
-    }
-    handleChange = ({ target:{value} }) => {
-        this.setState({value})
-    }
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.handleSearch(this.state.value)
-    }
-    render() {
-		return (
-			<>
-				<form
-					className='d-flex mt-2'
-					role='search'
-					onSubmit={this.handleSubmit}
-				>
-					<input
-						className='form-control me-2 '
-						type='search'
-						placeholder='Search'
-						aria-label='Search'
-                        onChange={this.handleChange}
-                        value={this.state.value}
-					/>
-					<button className='btn btn-outline-success' type='submit'>
-						Search
-					</button>
-				</form>
-			</>
-		)
-	}
-}
+  return (
+    <>
+      <form className="d-flex mt-2" role="search" onSubmit={handleSubmit}>
+        <input
+          className="form-control me-2 "
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          onChange={handleChange}
+          value={value}
+        />
+        <button className="btn btn-outline-success" type="submit">
+          Search
+        </button>
+      </form>
+    </>
+  );
+};
 
-export default Search
+// class Search extends Component{
+//     state = {
+//         value: ''
+//     }
+//     handleChange = ({ target:{value} }) => {
+//         this.setState({value})
+//     }
+//     handleSubmit = (e) => {
+//         e.preventDefault();
+//         this.props.handleSearch(this.state.value)
+//     }
+//     render() {
+// 		return (
+// 			<>
+// 				<form
+// 					className='d-flex mt-2'
+// 					role='search'
+// 					onSubmit={this.handleSubmit}
+// 				>
+// 					<input
+// 						className='form-control me-2 '
+// 						type='search'
+// 						placeholder='Search'
+// 						aria-label='Search'
+//                         onChange={this.handleChange}
+//                         value={this.state.value}
+// 					/>
+// 					<button className='btn btn-outline-success' type='submit'>
+// 						Search
+// 					</button>
+// 				</form>
+// 			</>
+// 		)
+// 	}
+// }
+
+export default Search;
